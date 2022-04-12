35050ff2e70ab652310e9d566fff002d17f39c4e
Tucker <tucker.chaisit@gmail.com>
2022-04-12 00:32:50 -0400

    add grocery list model

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,6 @@
 #models 
+from cgitb import text
+from turtle import title
 from myapp import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 #allows to set up isAuthenticate etc 
@@ -18,6 +20,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    grocery = db.relationship('Grocery', backref='author', lazy=True)
 
     def __init__(self, email, username, password):
         self.email = email
@@ -31,3 +34,19 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"Username {self.username}"
 
+class Grocery(db.Model):
+    __tablename__ = 'grocerys'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    unit = db.Column(db.String(140), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, name, quantity, unit, user_id):
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"Post ID: {self.id} -- Name: {self.name} --- Quantity: {self.quantity}"
